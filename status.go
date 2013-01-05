fbfe7ede4b37dd74b30bf2cbb5ef5e174e4f53e1
Mikolaj Habryn <dichro@rcpt.to>
2013-01-05 15:20:48 -0500

    flesh out most of the rest

--- status.go
+++ status.go
@@ -27,8 +27,21 @@ type Light struct {
 	Type string `json:"type"`
 	Name string `json:"name"`
 	Model string `json:"modelid"`
-	SoftwareVer string `json:"swversion"`
+	SoftwareVersion string `json:"swversion"`
 	// what's a pointsymbol?
+}
+
+type ACL struct {
+	Last string `json:"last use date"`
+	Created string `json:"create date"`
+	Name string `json:"name"`
+}
+
+type Update struct {
+	State int `json:"updatestate"`
+	URL string `json:"url"`
+	Text string `json:"text"`
+	Notify bool `json:"notify"`
 }
 
 type Config struct {
@@ -41,12 +54,18 @@ type Config struct {
 	ProxyIP string `json:"proxyaddress"`
 	ProxyPort int `json:"proxyport"`
 	UTC string
+	ACL map[string]ACL `json:"whitelist"`
+	SoftwareVersion string `json:"swversion"`
+	Update Update `json:"swupdate"`
+	LinkButton bool `json:"linkbutton"`
+	PortalServices bool `json:"portalservices"`
 }
 
 type Status struct {
 	Lights map[string]Light `json:"lights"`
 	// TODO(dichro): groups
 	Config Config `json:"config"`
+	// TODO(dichro): schedules
 }
 	
 func (h *Hub) Status() {
