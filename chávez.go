a8e364e887b4291816aaf58780516db926b36145
Mikolaj Habryn <dichro@rcpt.to>
2013-01-15 23:56:27 -0500

    RESTful handler

--- chávez.go
+++ chávez.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"regexp"
 
 	"github.com/dichro/huego"
 )
@@ -12,8 +13,44 @@ var (
 	ok = []byte("OK")
 	username = flag.String("username", "", "username for Hue hub")
 	address  = flag.String("address", "", "address of Hue hub")
+	re = regexp.MustCompile("^/lights/([A-Za-z0-9' ]+)/([0-9]+)$")
 )
 
+
+func tristateLivingRoom(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/plain")
+	groups := re.FindStringSubmatch(req.URL.Path)
+	if len(groups) != 3 {
+		w.Write([]byte("bad"))
+		return
+	}
+	name, state := groups[1], groups[2]
+	hub := &huego.Hub{
+		Username: *username,
+		Address:  *address,
+	}
+	status, err := hub.Status()
+	if err != nil {
+		w.Write([]byte(err.Error()))
+		return
+	}
+	found := false
+	for key, light := range status.Lights {
+		if light.Name != name {
+			continue
+		}
+		found = true
+		s := light.State
+		s.On = state != "0"
+		hub.SetLightState(key, s)
+		break
+	}
+	if found {
+		w.Write(ok)
+	} else {
+		w.Write([]byte("not found"))
+	}
+}
 
 // call to signal that motion has been detected at the front door.
 func motionAtEntry(w http.ResponseWriter, req *http.Request) {
@@ -43,6 +80,7 @@ func allOff(w http.ResponseWriter, req *http.Request) {
 
 func main() {
 	flag.Parse()
+	http.HandleFunc("/lights/", tristateLivingRoom)
 	http.HandleFunc("/motionAtEntry", motionAtEntry)
 	http.HandleFunc("/allOff", allOff)
 	log.Printf("About to listen on 10443. Go to https://127.0.0.1:10443/")
